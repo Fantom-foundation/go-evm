4d3423fc87a486705a045e900548f83cd816c5fd
Andre Cronje <andre@mfin.io>
2018-08-09 09:46:59 +0200

    nil
    Signed-off-by: Andre Cronje <andre@mfin.io>

--- service/http_handlers.go
+++ service/http_handlers.go
@@ -311,7 +311,7 @@ func transactionReceiptHandler(w http.ResponseWriter, r *http.Request, m *Servic
 		ContractAddress:   receipt.ContractAddress,
 		Logs:              receipt.Logs,
 		LogsBloom:         receipt.Bloom,
-		Failed:            uint64(0),
+		Failed:            nil,
 	}
 
 	if receipt.Logs == nil {
